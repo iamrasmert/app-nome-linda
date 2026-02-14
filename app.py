ca0595a5e7ed34863b1ed324218e06139e15d8e5
iamrasmert <130160199+iamrasmert@users.noreply.github.com>
2026-02-14 14:25:40 -0300

    Update success message for consistency and clarity

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ linda = ["leticia", "letícia", "Leticia", "Letícia", "lele", "lelê"]
 
 if nome:
     if nome in linda:
-        st.success("Esse é o nome do amor da minha vida, a pessoa mais linda desse mundo que eu amo tanto!, te amo muito linda❤, se eu pudesse te dar o mundo eu daria, espero um dia formarmos nossa familia e sermos muito felizes junto, Eu Te Amo!")
+        st.success("Esse é o nome do amor da minha vida, a pessoa mais linda desse mundo que eu amo tanto!, te amo muito linda❤, se eu pudesse te dar o mundo eu daria, um dia vamos ter nossa família e seremos muito felizes junto, Eu Te Amo!")
     else:
         st.warning("Nomezinho mais ou menos 😅")
